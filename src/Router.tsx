c82ecce6e390403f4e91832cf2226b2c9ff3d3e4
rlaisqls <rlaisqls@gmail.com>
2023-02-04 16:40:52 +0900

    feat: 개인정보 처리방침
    Revert "feat: 개인정보 처리방침"
    
    This reverts commit 1b2d767783fafb38ffd6ab96a7265f6e12b9464a.
    
    feat: 개인정보 처리방침

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Policy } from './pages/Policy';
+import { PrivacyPolicy } from './pages/PrivacyPolicy';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/sign-up-policy" element={<Policy />} />
+        <Route path="/policy/privacy" element={<PrivacyPolicy />} />
       </Routes>
     </BrowserRouter>
   );
