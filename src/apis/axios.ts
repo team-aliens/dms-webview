c503c74e3731ee5447f13334d16757f7e6915453
이의진 <aghpt04@gmail.com>
2024-10-20 19:52:05 +0900

    fix: conflict

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -25,42 +25,6 @@ instance.interceptors.request.use(
   (error: AxiosError) => Promise.reject(error),
 );
 
-<<<<<<< Updated upstream
-
-// instance.interceptors.response.use(
-//   (response) => response,
-//   (error: AxiosError<AxiosError>) => {
-//     if (axios.isAxiosError(error) && error.response) {
-//       const {config} = error;
-//       const refreshToken = getCookie('refresh_token');
-//       if (error.response.data.message === 'Expired Token') {
-//         if (refreshToken) {
-//           reIssueToken(refreshToken).then((res) => {
-//             const accessExpired = new Date(res.access_token_expired_at);
-//             const refreshExpired = new Date(res.refresh_token_expired_at);
-
-//             setCookie('access_token', res.access_token, {
-//               expires: accessExpired,
-//             });
-
-//             setCookie('refresh_token', res.refresh_token, {
-//               expires: refreshExpired,
-//             });
-
-//             if (config?.headers)
-//               config.headers['Authorization'] = `Bearer ${res.access_token}`
-//             return axios(config)
-//           });
-//         } else {
-//           window.location.href = '/login';
-//         }
-//       } else return Promise.reject(error)
-//     }
-//   }
-// );
-=======
->>>>>>> Stashed changes
-
 instance.interceptors.response.use(
   (response) => response,
   async (error: AxiosError<AxiosError>) => {
