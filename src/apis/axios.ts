67b445b255028a1581d1c1a625d4442c7b0bb561
seungW0o <ssounsowns@gmail.com>
2025-07-20 20:25:16 +0900

    fix : axios 로직 수정

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -10,12 +10,17 @@ export const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 10000,
 });
+
 let isRefreshing = false;
 let refreshSubscribers: ((token: string) => void)[] = [];
 
 const onRefreshed = (token: string) => {
   refreshSubscribers.forEach((callback) => callback(token));
   refreshSubscribers = [];
+};
+
+const subscribeTokenRefresh = (callback: (token: string) => void) => {
+  refreshSubscribers.push(callback);
 };
 
 instance.interceptors.request.use(
@@ -43,21 +48,25 @@ instance.interceptors.response.use(
       const refreshToken = getCookie('refresh_token');
 
       if (!refreshToken) {
+        console.warn('리프레시 토큰 없음. 로그인 필요함.');
         window.location.href = '/login';
         return Promise.reject(error);
       }
 
       if (originalRequest._retry) {
+        console.warn('이미 리트라이한 요청인데 실패함.');
+        window.location.href = '/login';
         return Promise.reject(error);
       }
+
       originalRequest._retry = true;
 
       if (isRefreshing) {
         return new Promise((resolve) => {
-          refreshSubscribers.push((token: string) => {
+          subscribeTokenRefresh((newToken) => {
             originalRequest.headers = new AxiosHeaders({
               ...originalRequest.headers,
-              Authorization: `Bearer ${token}`,
+              Authorization: `Bearer ${newToken}`,
             });
             resolve(instance(originalRequest));
           });
@@ -67,29 +76,36 @@ instance.interceptors.response.use(
       isRefreshing = true;
 
       try {
+        console.log('토큰 리프레시 요청');
         const res = await reIssueToken(refreshToken);
         const newAccessToken = res.access_token;
+        const newRefreshToken = res.refresh_token;
+
+        if (!newAccessToken || !newRefreshToken) {
+          throw new Error('리프레시 응답이 유효하지 않음');
+        }
 
         setCookie('access_token', newAccessToken, {
           expires: new Date(res.access_token_expired_at),
         });
 
-        setCookie('refresh_token', res.refresh_token, {
+        setCookie('refresh_token', newRefreshToken, {
           expires: new Date(res.refresh_token_expired_at),
         });
 
-        isRefreshing = false;
         onRefreshed(newAccessToken);
-
         originalRequest.headers = new AxiosHeaders({
           ...originalRequest.headers,
           Authorization: `Bearer ${newAccessToken}`,
         });
+
         return instance(originalRequest);
       } catch (err) {
-        isRefreshing = false;
+        console.error('토큰 재발급 실패:', err);
         window.location.href = '/login';
         return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
       }
     }
 
