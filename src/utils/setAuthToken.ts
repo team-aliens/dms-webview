f0ef9f7aecaf1da50956d5efb95a2d2964fffed1
seungW0o <ssounsowns@gmail.com>
2025-11-09 17:11:42 +0900

    fix: setAuthToken 쿠키 설정 로직 수정
    - 기존에는 쿠키 값에서 "value=" 부분을 추출하는 로직이 있었으나,
      더 이상 필요하지 않으므로 제거
    - accessToken과 refreshToken이 존재하는 경우에만 쿠키를 설정하도록 수정
      - 불필요한 쿠키 설정 방지

--- src/utils/setAuthToken.ts
+++ src/utils/setAuthToken.ts
@@ -1,10 +1,5 @@
 import { setCookie } from './cookies';
 
-function extractTokenValue(tokenString: string): string {
-  const match = tokenString.match(/value=([^,]+)/);
-  return match ? match[1] : tokenString;
-}
-
 export function registerSetAuthToken() {
   window.setAuthToken = (accessToken: string, refreshToken: string) => {
     console.log('setAuthToken 실행됨!', accessToken, refreshToken);
@@ -12,18 +7,15 @@ export function registerSetAuthToken() {
     const expires = new Date();
     expires.setDate(expires.getDate() + 7);
 
-    const actualAccessToken = extractTokenValue(accessToken);
-    const actualRefreshToken = extractTokenValue(refreshToken);
-
-    if (actualAccessToken) {
-      setCookie('access_token', actualAccessToken, {
+    if (accessToken) {
+      setCookie('access_token', accessToken, {
         path: '/',
         expires,
       });
     }
 
-    if (actualRefreshToken) {
-      setCookie('refresh_token', actualRefreshToken, {
+    if (refreshToken) {
+      setCookie('refresh_token', refreshToken, {
         path: '/',
         expires,
       });
